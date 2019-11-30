5b17dea84df399a60a1f508b757318ce59452b51
varn <varnbu@gmail.com>
2019-11-30 16:40:56 +0800

    天界方法

--- src/main/java/com/varn/springHello/HelloSpring/serviceImpl/HeroServiceImpl.java
+++ src/main/java/com/varn/springHello/HelloSpring/serviceImpl/HeroServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
-public class HeroService implements IHeroService {
+public class HeroServiceImpl implements IHeroService {
     @Autowired
     IHeroDao iHeroDao;
 
--- src/main/java/com/varn/springHello/HelloSpring/web/heroController.java
+++ src/main/java/com/varn/springHello/HelloSpring/web/heroController.java
@@ -4,6 +4,8 @@ import com.varn.springHello.HelloSpring.c.Hero;
 import com.varn.springHello.HelloSpring.service.IHeroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,12 +15,32 @@ import java.util.List;
 public class heroController {
     @Autowired
     IHeroService iHeroService;
+
     @RequestMapping("/test")
     public String test() {
         return "hero test";
     }
+
     @RequestMapping("list")
-    public List<Hero> list(){
+    public List<Hero> list() {
         return iHeroService.findHeroList();
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public int add(@RequestParam(value = "name") String name,
+                   @RequestParam(value = "sex") int sex,
+                   @RequestParam(value = "areas") int areas,
+                   @RequestParam(value = "speed") int speed) {
+        Hero hero = new Hero();
+        hero.setName(name);
+        hero.setSex(sex);
+        hero.setAreas(areas);
+        hero.setSpeed(speed);
+        int res = iHeroService.add(hero);
+        if (res == 1) {
+            return 9;
+        } else {
+            return 0;
+        }
     }
 }
