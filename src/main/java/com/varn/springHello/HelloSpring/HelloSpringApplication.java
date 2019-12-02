7ba58d795f61e9a2d7fe894b82958102e2defa8a
varn <varnbu@gmail.com>
2019-12-02 17:18:05 +0800

    test 不行

--- src/main/java/com/varn/springHello/HelloSpring/HelloSpringApplication.java
+++ src/main/java/com/varn/springHello/HelloSpring/HelloSpringApplication.java
@@ -1,9 +1,14 @@
 package com.varn.springHello.HelloSpring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @RestController
@@ -15,6 +20,19 @@ public class HelloSpringApplication {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "hello ";
+        return "hello";
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("========================================> inspect beans:");
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            System.out.println("数量： " + beanNames.length);
+            for (String name : beanNames) {
+//                System.out.println(name);
+            }
+        };
     }
 }
--- src/test/java/com/varn/springHello/HelloSpring/HelloSpringApplicationTests.java
+++ src/test/java/com/varn/springHello/HelloSpring/HelloSpringApplicationTests.java
@@ -1,13 +1,35 @@
 package com.varn.springHello.HelloSpring;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.net.URL;
+
+import static com.sun.javafx.fxml.expression.Expression.equalTo;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class HelloSpringApplicationTests {
+    @LocalServerPort
+    private int port;
 
-	@Test
-	void contextLoads() {
-	}
+    private URL base;
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Test
+    void contextLoads() {
+    }
+
+    public void getHello() throws Exception {
+        ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
+//        assertThat(response.getBody(), equalTo("hello"));
+    }
 
 }
