5e58022d15a222b3434aaea1daacc6ab1859d8a0
varn <varnbu@gmail.com>
2019-12-03 10:55:11 +0800

    junit 测试添加

--- src/test/java/com/varn/springHello/HelloSpring/HelloSpringApplicationTests.java
+++ src/test/java/com/varn/springHello/HelloSpring/HelloSpringApplicationTests.java
@@ -3,17 +3,20 @@ package com.varn.springHello.HelloSpring;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.net.URL;
 
-
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HelloSpringApplicationTests {
     @LocalServerPort
     private int port;
@@ -23,12 +26,17 @@ class HelloSpringApplicationTests {
     @Autowired
     private TestRestTemplate template;
 
-    @Test
-    void contextLoads() {
+    @BeforeEach
+    public void setUp() throws Exception {
+        this.base = new URL("http://localhost:" + this.port + "/hello");
+        System.out.println("before : "+this.base);
     }
 
+    @Test
     public void getHello() throws Exception {
-        ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
+        System.out.println(template);
+        System.out.println(this.base);
+        ResponseEntity<String> response = template.getForEntity(base.toString(),String.class);
         assertThat(response.getBody(), equalTo("hello"));
     }
 
