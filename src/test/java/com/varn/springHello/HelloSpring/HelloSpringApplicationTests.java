26be114a85f3cc0940d39772277b909dd7b4e6c1
varn <varnbu@gmail.com>
2019-12-03 09:44:38 +0800

    静态页面访问

--- src/test/java/com/varn/springHello/HelloSpring/HelloSpringApplicationTests.java
+++ src/test/java/com/varn/springHello/HelloSpring/HelloSpringApplicationTests.java
@@ -9,12 +9,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.net.URL;
 
-import static com.sun.javafx.fxml.expression.Expression.equalTo;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class HelloSpringApplicationTests {
