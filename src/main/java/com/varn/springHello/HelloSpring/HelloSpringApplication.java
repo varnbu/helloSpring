89103d82d642918ffcd8fbaecb61456dfc3b14a8
varn <varnbu@gmail.com>
2019-12-06 11:42:06 +0800

    开启定时任务

--- src/main/java/com/varn/springHello/HelloSpring/HelloSpringApplication.java
+++ src/main/java/com/varn/springHello/HelloSpring/HelloSpringApplication.java
@@ -11,12 +11,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 
 @SpringBootApplication
+@EnableScheduling
 @RestController
 public class HelloSpringApplication {
     @Value("${my.name}")
---
+++ src/main/java/com/varn/springHello/HelloSpring/schedules/ScheduledTasks.java
@@ -0,0 +1,20 @@
+package com.varn.springHello.HelloSpring.schedules;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class ScheduledTasks {
+    public static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 1000)
+    public void SecondSayHello() {
+        log.info("hello: time at {}", dateFormat.format(new Date()));
+    }
+}
