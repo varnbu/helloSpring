df7fc703d583545b53e63313e0baf0d63abb0322
varn <varnbu@gmail.com>
2019-12-06 11:42:15 +0800

    开启定时任务

--- src/main/java/com/varn/springHello/HelloSpring/schedules/ScheduledTasks.java
+++ src/main/java/com/varn/springHello/HelloSpring/schedules/ScheduledTasks.java
@@ -13,7 +13,7 @@ public class ScheduledTasks {
     public static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    @Scheduled(fixedRate = 1000)
+    @Scheduled(fixedRate = 60000)
     public void SecondSayHello() {
         log.info("hello: time at {}", dateFormat.format(new Date()));
     }
