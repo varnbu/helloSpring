3b0e538a76a319dd09b1714cdc0d551be88f7b8e
varn <varnbu@gmail.com>
2019-12-06 11:27:03 +0800

    文件列表返回

--- src/main/java/com/varn/springHello/HelloSpring/web/HeroFileController.java
+++ src/main/java/com/varn/springHello/HelloSpring/web/HeroFileController.java
@@ -5,9 +5,14 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -25,9 +30,19 @@ public class HeroFileController {
         return "file test";
     }
 
+    @GetMapping("/")
+    public String listUploadFiles(Model model) throws IOException {
+        model.addAttribute("files", storageService.loadAll()
+                .map(path ->
+                        MvcUriComponentsBuilder.fromMethodName(HeroFileController.class, "serveFile", path.getFileName().toString())
+                                .build().toString())
+                .collect(Collectors.toList()));
+        return model.toString();
+    }
+
     @GetMapping("/{filename:.+}")
     @ResponseBody
-    public ResponseEntity<Resource> saveFile(@PathVariable String filename) {
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
         Resource file = storageService.loadAsResource(filename);
         return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG)
                 .header(HttpHeaders.CONTENT_DISPOSITION, ";filename=\"" + file.getFilename() + "\"")
