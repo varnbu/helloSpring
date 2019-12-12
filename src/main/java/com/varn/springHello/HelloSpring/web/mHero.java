4a5bcee60a830acbfd9af531622bff16734abef8
varn <varnbu@gmail.com>
2019-12-12 17:48:58 +0800

    devtools 的使用

--- src/main/java/com/varn/springHello/HelloSpring/web/mHero.java
+++ src/main/java/com/varn/springHello/HelloSpring/web/mHero.java
@@ -2,7 +2,6 @@ package com.varn.springHello.HelloSpring.web;
 
 import com.varn.springHello.HelloSpring.c.Hero;
 import com.varn.springHello.HelloSpring.service.MHeroService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
