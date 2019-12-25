0f047d92eefdaabd62a1daac4a12899528aa0481
varn <varnbu@gmail.com>
2019-12-25 17:20:34 +0800

    请求提的默认参数

--- src/main/java/com/varn/springHello/HelloSpring/web/HtmlController.java
+++ src/main/java/com/varn/springHello/HelloSpring/web/HtmlController.java
@@ -3,12 +3,13 @@ package com.varn.springHello.HelloSpring.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HtmlController {
     @RequestMapping("/result")
-    public String result(Model model) {
-        model.addAttribute("message", "from controller");
+    public String result(@RequestParam(defaultValue = "world") String name, Model model) {
+        model.addAttribute("message", name+" say hello");
         return "result";
     }
 }
